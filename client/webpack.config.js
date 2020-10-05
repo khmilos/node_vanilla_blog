f85eb780b82d38ec0b6a04dff1a23dc04a079dc6
khmilos <khmil.oleksandr32@gmail.com>
2020-10-05 20:59:30 +0300

    client app basic configuration

---
+++ client/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path')
+
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = (env, argv) => ({
+  entry: ['./src/js/index.js', './src/sass/index.sass'],
+  output: {
+    filename: '[name].boundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  },
+  devtool: argv.mode === 'development' ? 'inline-source-map' : false,
+  plugins: [
+    new CleanWebpackPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/templates' },
+        { from: 'src/static', to: 'static' }
+      ]
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(css|sass|scss)$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.m?js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        loader: 'file-loader',
+        options: {
+          outputPath: 'static',
+          name: '[name].[ext]'
+        }
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        use: 'file-loader'
+      }
+    ]
+  }
+})
